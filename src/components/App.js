2ea692f50559edba2497d574026e8590b28d2a04
uanoob <usnet@ukr.net>
2018-12-10 17:20:47 +0200

    Move to components folder -> App.js App.test.js

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  Route, Switch, Redirect, withRouter,
+} from 'react-router-dom';
+import WelcomePage from './WelcomePage';
+import ChatPage from './ChatPage';
+import { authCheck } from '../store/actions';
+
+class App extends React.Component {
+  componentDidMount() {
+    const { onAuthCheck } = this.props;
+    onAuthCheck();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props;
+    let routes = (
+      <Switch>
+        <Route exact path="/welcome" component={WelcomePage} />
+        <Redirect to="/welcome" />
+      </Switch>
+    );
+    if (isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route exact path="/chat" component={ChatPage} />
+          <Redirect to="/chat" />
+        </Switch>
+      );
+    }
+    return <Fragment>{routes}</Fragment>;
+  }
+}
+App.propTypes = {
+  onAuthCheck: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+const mapDispatchToProps = {
+  onAuthCheck: authCheck,
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(App),
+);
