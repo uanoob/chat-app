2f41db49e6c8f7950c18a7eda17b89ee800d73c3
uanoob <usnet@ukr.net>
2018-12-11 14:03:13 +0200

    Move ChatList & ChatListItem to separate components

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -5,14 +5,9 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import ChatList from './ChatList';
 
 const drawerWidth = 240;
 
@@ -51,7 +46,7 @@ class PermanentDrawerLeft extends React.Component {
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
             <Typography variant="h6" color="inherit" noWrap>
-              Permanent drawer
+              Chat App
             </Typography>
           </Toolbar>
         </AppBar>
@@ -65,27 +60,7 @@ class PermanentDrawerLeft extends React.Component {
         >
           <div className={classes.toolbar} />
           <Divider />
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
+          <ChatList />
         </Drawer>
         <main className={classes.content}>
           <div className={classes.toolbar} />
