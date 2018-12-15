595b8bdf4528888f543b97166291f2c3dcd41266
uanoob <usnet@ukr.net>
2018-12-15 16:03:48 +0200

    Add checking for empty message list

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core';
 import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
 import MessageListItem from './MessageListItem';
 
 const styles = theme => ({
@@ -10,15 +11,18 @@ const styles = theme => ({
   },
 });
 
-const MessageList = ({ classes, messages, userId }) => messages
-  && messages.length !== 0 && (
+const MessageList = (props) => {
+  const { classes, messages, userId } = props;
+  return messages && messages.length !== 0 ? (
     <List className={classes.container} component="nav">
       {messages.map(message => (
         <MessageListItem key={message._id} message={message} userId={userId} />
       ))}
     </List>
-);
-
+  ) : (
+    <Typography variant="display1">There is no messages yet...</Typography>
+  );
+};
 MessageList.propTypes = {
   classes: PropTypes.shape({
     container: PropTypes.string.isRequired,
@@ -35,9 +39,10 @@ MessageList.propTypes = {
         username: PropTypes.string.isRequired,
         lastName: PropTypes.string.isRequired,
         firstName: PropTypes.string.isRequired,
-      }),
+      }).isRequired,
     }),
-  ),
+  ).isRequired,
+  userId: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(MessageList);
