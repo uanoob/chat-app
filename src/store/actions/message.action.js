d81ce012924ff2291413b5fd53103e0f26b10372
uanoob <usnet@ukr.net>
2018-12-15 16:00:29 +0200

    Move implementation to -> socket

--- src/store/actions/message.action.js
+++ src/store/actions/message.action.js
@@ -1,49 +1,49 @@
-import {
-  SEND_MESSAGE_START,
-  SEND_MESSAGE_SUCCESS,
-  SEND_MESSAGE_FAIL,
-} from './types';
+// import {
+//   SEND_MESSAGE_START,
+//   SEND_MESSAGE_SUCCESS,
+//   SEND_MESSAGE_FAIL,
+// } from './types';
 
-import BASE_URL from '../../config';
-import { getChatById } from '.';
+// import BASE_URL from '../../config';
+// import { getChatById } from '.';
 
-const sendMessageStart = () => ({
-  type: SEND_MESSAGE_START,
-});
+// const sendMessageStart = () => ({
+//   type: SEND_MESSAGE_START,
+// });
 
-export const sendMessageSuccess = data => ({
-  type: SEND_MESSAGE_SUCCESS,
-  payload: data,
-});
+// export const sendMessageSuccess = data => ({
+//   type: SEND_MESSAGE_SUCCESS,
+//   payload: data,
+// });
 
-const sendMessageFail = error => ({
-  type: SEND_MESSAGE_FAIL,
-  payload: error,
-});
+// const sendMessageFail = error => ({
+//   type: SEND_MESSAGE_FAIL,
+//   payload: error,
+// });
 
-export const sendMessage = (message, chatId) => (dispatch, getState) => {
-  dispatch(sendMessageStart());
-  const { token } = getState().auth;
-  fetch(`${BASE_URL}/chats/${chatId}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      data: { content: message },
-    }),
-  })
-    .then(response => response.json())
-    .then((data) => {
-      if (data.success) {
-        dispatch(sendMessageSuccess(data));
-        dispatch(getChatById(chatId));
-      } else {
-        dispatch(sendMessageFail(data));
-      }
-    })
-    .catch((error) => {
-      dispatch(sendMessageFail(error));
-    });
-};
+// export const sendMessage = (message, chatId) => (dispatch, getState) => {
+//   dispatch(sendMessageStart());
+//   const { token } = getState().auth;
+//   fetch(`${BASE_URL}/chats/${chatId}`, {
+//     method: 'POST',
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       data: { content: message },
+//     }),
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       if (data.success) {
+//         dispatch(sendMessageSuccess(data));
+//         dispatch(getChatById(chatId));
+//       } else {
+//         dispatch(sendMessageFail(data));
+//       }
+//     })
+//     .catch((error) => {
+//       dispatch(sendMessageFail(error));
+//     });
+// };
--- src/store/reducers/message.reducer.js
+++ src/store/reducers/message.reducer.js
@@ -1,37 +1,37 @@
-import {
-  SEND_MESSAGE_START,
-  SEND_MESSAGE_SUCCESS,
-  SEND_MESSAGE_FAIL,
-} from '../actions/types';
+// import {
+//   SEND_MESSAGE_START,
+//   SEND_MESSAGE_SUCCESS,
+//   SEND_MESSAGE_FAIL,
+// } from '../actions/types';
 
-const initialState = {
-  message: '',
-  loading: false,
-  loaded: false,
-};
+// const initialState = {
+//   message: '',
+//   loading: false,
+//   loaded: false,
+// };
 
-export default function (state = initialState, action) {
-  switch (action.type) {
-    case SEND_MESSAGE_START:
-      return {
-        ...state,
-        loading: true,
-      };
-    case SEND_MESSAGE_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        loaded: action.payload.success,
-        message: action.payload.message,
-      };
-    case SEND_MESSAGE_FAIL:
-      return {
-        ...state,
-        message: action.payload.message,
-        loading: false,
-        loaded: false,
-      };
-    default:
-      return state;
-  }
-}
+// export default function (state = initialState, action) {
+//   switch (action.type) {
+//     case SEND_MESSAGE_START:
+//       return {
+//         ...state,
+//         loading: true,
+//       };
+//     case SEND_MESSAGE_SUCCESS:
+//       return {
+//         ...state,
+//         loading: false,
+//         loaded: action.payload.success,
+//         message: action.payload.message,
+//       };
+//     case SEND_MESSAGE_FAIL:
+//       return {
+//         ...state,
+//         message: action.payload.message,
+//         loading: false,
+//         loaded: false,
+//       };
+//     default:
+//       return state;
+//   }
+// }
