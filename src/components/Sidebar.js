b8a9c9a5bd9c9c7ff62925fbf3c84dfca480f9be
uanoob <usnet@ukr.net>
2018-12-18 13:00:27 +0200

    Add search & filters all chats

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui//core/TextField';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import ChatList from './chats/ChatList';
 import ChatForm from './chats/ChatForm';
+import { getAllChats } from '../store/actions';
 
 const drawerWidth = 300;
 
@@ -17,6 +20,11 @@ const styles = theme => ({
   },
   drawerPaper: {
     width: drawerWidth,
+  },
+  drawerHeader: {
+    ...theme.mixins.toolbar,
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
   },
   toolbar: theme.mixins.toolbar,
   fab: {
@@ -29,7 +37,13 @@ const styles = theme => ({
 export class SidebarComponent extends React.Component {
   state = {
     chatDialog: false,
+    searchValue: '',
   };
+
+  componentDidMount() {
+    const { onGetAllChats } = this.props;
+    onGetAllChats();
+  }
 
   handleChatDialogClick = () => {
     const { chatDialog } = this.state;
@@ -38,9 +52,25 @@ export class SidebarComponent extends React.Component {
     });
   };
 
+  handleSearchChange = (event) => {
+    this.setState({
+      searchValue: event.target.value,
+    });
+  };
+
+  filterChats = (chats) => {
+    const { searchValue } = this.state;
+    return chats
+      .filter(chat => chat.title.toLowerCase()
+        .includes(searchValue.toLowerCase()))
+      .sort(
+        (a, b) => (a.title.toLowerCase() <= b.title.toLowerCase() ? -1 : 1),
+      );
+  };
+
   render() {
-    const { classes } = this.props;
-    const { chatDialog } = this.state;
+    const { classes, chats } = this.props;
+    const { chatDialog, searchValue } = this.state;
     return (
       <Drawer
         className={classes.drawer}
@@ -50,9 +80,17 @@ export class SidebarComponent extends React.Component {
         }}
         anchor="left"
       >
-        <div className={classes.toolbar} />
+        <div className={classes.drawerHeader}>
+          <TextField
+            fullWidth
+            margin="normal"
+            placeholder="Search chats..."
+            value={searchValue}
+            onChange={this.handleSearchChange}
+          />
+        </div>
         <Divider />
-        <ChatList />
+        <ChatList chats={this.filterChats(chats)} />
         <Fab
           className={classes.fab}
           color="primary"
@@ -71,15 +109,47 @@ export class SidebarComponent extends React.Component {
   }
 }
 
+SidebarComponent.defaultProps = {
+  chats: [],
+};
+
 SidebarComponent.propTypes = {
   classes: PropTypes.shape({
     drawer: PropTypes.string.isRequired,
     drawerPaper: PropTypes.string.isRequired,
+    drawerHeader: PropTypes.string.isRequired,
     toolbar: PropTypes.string.isRequired,
     fab: PropTypes.string.isRequired,
   }).isRequired,
+  chats: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      updatedAt: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      creator: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        firstName: PropTypes.string.isRequired,
+      }),
+      title: PropTypes.string.isRequired,
+      members: PropTypes.arrayOf(PropTypes.string.isRequired),
+    }),
+  ),
+  onGetAllChats: PropTypes.func.isRequired,
 };
 
-const Sidebar = withStyles(styles)(SidebarComponent);
+const mapStateToProps = state => ({
+  chats: state.chats.chats,
+});
+
+const mapDispatchToProps = {
+  onGetAllChats: getAllChats,
+};
+
+const Sidebar = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withStyles(styles)(SidebarComponent));
 
 export default Sidebar;
